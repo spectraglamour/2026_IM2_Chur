0c5e14735fe59c9d52cd3f1b236ffc1ad3f85ebe
spectraglamour <leonie.eigenheer@stud.fhgr.ch>
2026-05-20 09:58:23 +0200

    api eingebunden java script

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
-console.log("blub");
+const url = "https://extinct-api.herokuapp.com/api/v1/animal/804";
+try {
+  const response = await fetch(url);
+  const data = await response.json();
+  console.log(data); // gibt die Daten der API in der Konsole aus
+} catch (error) {
+  console.error(error);
+}
