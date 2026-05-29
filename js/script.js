b343d9a2b226c248537d6324fae91d014003d5b0
Nyahlina <nyah.plozza@stud.fhgr.ch>
2026-05-29 20:41:00 +0200

    Update 29. Mai

--- js/script.js
+++ js/script.js
@@ -5,3 +5,23 @@ if (heartWrap) {
     window.location.href = "regretlist.html";
   });
 }
+
+const regretIcon = document.querySelector("#regretIcon");
+
+if (regretIcon) {
+  regretIcon.addEventListener("click", () => {
+    const isSaved = regretIcon.dataset.saved === "true";
+
+    if (isSaved) {
+      regretIcon.src = regretIcon.dataset.white;
+      regretIcon.dataset.saved = "false";
+    } else {
+      regretIcon.src = regretIcon.dataset.black;
+      regretIcon.dataset.saved = "true";
+    }
+  });
+}
+
+killButton.addEventListener("click", () => {
+  console.log("kill again");
+});
